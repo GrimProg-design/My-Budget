b82f60ce8520ab8f71af9351b922665e22c72ee8
GrimProg <vavilovila01@gmail.com>
2026-02-23 19:33:52 +0600

    Initial Commit

---
+++ My Budget/My_BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_BudgetApp.swift
+//  My Budget
+//
+//  Created by Вавилов Илья on 23/2/26.
+//
+
+import SwiftUI
+
+@main
+struct My_BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ My BudgetTests/My_BudgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  My_BudgetTests.swift
+//  My BudgetTests
+//
+//  Created by Вавилов Илья on 23/2/26.
+//
+
+import Testing
+@testable import My_Budget
+
+struct My_BudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
