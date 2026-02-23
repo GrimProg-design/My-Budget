963ecc06c7edb25a973a1e87a74924e69c479d51
GrimProg <vavilovila01@gmail.com>
2026-02-23 21:53:33 +0600

    создал кастомный таб бар, является не законченным но нужен комит

--- My Budget/View/CustomTabBar.swift
+++ My Budget/View/CustomTabBar.swift
@@ -7,9 +7,71 @@
 
 import SwiftUI
 
+enum Tab: String, CaseIterable {
+    case home, laboratory
+    
+    var title: String  {
+        switch self {
+        case .home:
+            return "Главная"
+        case .laboratory:
+            return "Лаборатория"
+        }
+    }
+    
+    var icon: String {
+        switch self {
+        case .home:
+            return "house"
+        case .laboratory:
+            return "flask"
+        }
+    }
+}
+
 struct CustomTabBar: View {
+    @State private var selectedTab: Tab = .home
+    @State private var check = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationStack {
+            switch selectedTab {
+            case .home:
+                HomeView()
+            case .laboratory:
+                Text("Hello")
+            }
+        }
+        
+        Text(check ? "Hello" : "Bye")
+        Spacer()
+        HStack {
+            ForEach(Tab.allCases, id: \.self) { tab in
+                Spacer()
+                Button {
+                    selectedTab = tab
+                    check.toggle()
+                } label: {
+                    VStack {
+                        Image(systemName: tab.icon)
+                        Text(tab.title)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .contentShape(Rectangle())
+                }
+                Spacer()
+                
+                if tab != Tab.allCases.last {
+                    Divider()
+                        .frame(maxHeight: 20)
+                        .background(.black)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(.ultraThinMaterial)
     }
 }
 
