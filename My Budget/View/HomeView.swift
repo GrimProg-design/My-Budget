855b642b9d273816f9fd1376fd4abb2ac97d93e7
GrimProg <vavilovila01@gmail.com>
2026-02-23 21:52:54 +0600

    Создал кастомный таб бар внизу экрана, является не законченным, но коммит нужен

---
+++ My Budget/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  My Budget
+//
+//  Created by Вавилов Илья on 23/2/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
