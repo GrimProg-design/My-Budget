50e6e731f0dff82320b5ebccc1c900115a25ee79
GrimProg <vavilovila01@gmail.com>
2026-02-24 22:18:31 +0600

    Создал базовую структуру главного меню а так же добавил некоторый контент (надпись "деньги" и дни недели в горизонтальной прокрутке)

--- My Budget/View/HomeView.swift
+++ My Budget/View/HomeView.swift
@@ -9,7 +9,73 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Hello, from home")
+        VStack {
+            HStack {
+                Text("money")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                Text("per day")
+                    .foregroundStyle(.gray)
+                    
+            }
+            .padding(60)
+            
+            Spacer()
+            
+            VStack {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 15) {
+                        ForEach(Weekdays.allCases, id: \.self) {day in
+                            Button {
+                                
+                            } label: {
+                                Text(day.daysInWeek)
+                                    .font(.title2)
+                                    .padding(8)
+                                    .foregroundStyle(.black)
+                            }
+                            .background(.ultraThinMaterial)
+                            .cornerRadius(20)
+                            .padding(9)
+                            .shadow(radius: 5)
+                        }
+                    }
+                }
+                .padding()
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: 600)
+            .background(.white)
+            .clipShape(
+                UnevenRoundedRectangle(topLeadingRadius: 30, topTrailingRadius: 30
+                                      )
+            )
+            .ignoresSafeArea()
+        }
+        .background(LinearGradient(colors: [Color("AncientGreen"), .white], startPoint: .top, endPoint: .bottom))
+    }
+}
+
+enum Weekdays: String, CaseIterable {
+    case monday, tuesday, wednesday, thursday, friday, saturday, sunday
+    
+    var daysInWeek: String {
+        switch self {
+        case .monday:
+        "Понедельник"
+        case .tuesday:
+            "Вторник"
+        case .wednesday:
+            "Среда"
+        case .thursday:
+            "Четверг"
+        case .friday:
+            "Пятница"
+        case .saturday:
+            "Суббота"
+        case .sunday:
+            "Воскресенье"
+        }
     }
 }
 
