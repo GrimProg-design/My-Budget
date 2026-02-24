d601a5584ba8ba35b29666e91dc7d79e416e4ae2
GrimProg <vavilovila01@gmail.com>
2026-02-24 20:24:41 +0600

    Создал вьюху лаборатории

---
+++ My Budget/View/LaboratoryView.swift
@@ -0,0 +1,18 @@
+//
+//  LaboratoryView.swift
+//  My Budget
+//
+//  Created by Вавилов Илья on 24/2/26.
+//
+
+import SwiftUI
+
+struct LaboratoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaboratoryView()
+}
